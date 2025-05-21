c9e165415adaba9c0cf57e599533ee1b2098a9ad
Javier Gonzalez <jgonz0010@gmail.com>
2025-05-21 17:55:54 -0500

    cleaning up

--- rss_reader/config/reader_config.py
+++
@@ -1,5 +0,0 @@
-class ReaderConfig(object):
-    def __init__(self):
-        self.feed_urls = list(open("rss_list.cfg", "r").readlines())
-        self.character_speed = 75
-        self.line_speed = 250
